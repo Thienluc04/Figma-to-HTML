524276a9e51d542995aeb741096fd1111401c27b
Luc <nguyenthienluc2004@gmail.com>
2021-11-24 08:21:16 +0700

    firts commit

---
+++ Trafalgar/js/app.js
@@ -0,0 +1,8 @@
+$('.customer__content').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    prevArrow:`<button type='button' class='slick-arrow arrow-left'><img src="img/prev-arrow.svg"></button>`,
+    nextArrow:`<button type='button' class='slick-arrow arrow-right'><img src="img/next-arrow.svg"></button>`
+  });
